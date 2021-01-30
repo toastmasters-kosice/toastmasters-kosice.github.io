c7bd1ebabefe9f2554f4e19c49a837ed7a501bb7
Vladimir Zahradnik <vladimir@zahradnik.io>
2021-01-30 15:42:01 +0100

    Remove Netlify dependencies

--- gatsby-config.js
+++ gatsby-config.js
@@ -86,19 +86,12 @@ module.exports = {
       resolve: "gatsby-plugin-sitemap"
     },
     {
-      resolve: 'gatsby-plugin-netlify-cms',
-      options: {
-        modulePath: `${__dirname}/src/cms/cms.js`,
-      },
-    },
-    {
       resolve: 'gatsby-plugin-purgecss', // purges all unused/unreferenced css rules
       options: {
         develop: true, // Activates purging in npm run develop
         whitelist: ['video-container', 'video-iframe'],
         purgeOnly: ['/all.sass'], // applies purging only on the bulma css file
       },
-    }, // must be after other CSS plugins
-    'gatsby-plugin-netlify', // make sure to keep it last in the array
+    },
   ],
 }
