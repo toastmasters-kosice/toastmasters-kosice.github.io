5f4d9659d114b159c48fdfdfa8fb960139468dc0
Dabuvid <137901854+Dabuvid@users.noreply.github.com>
2024-02-10 12:15:07 +0100

    Update 2024-02-05-Gesta.m

--- src/pages/blog/2024-02-05-Gesta.m
+++ src/pages/blog/2024-02-05-Gesta.m
@@ -17,32 +17,32 @@ tags:
 
 ![Interview](/)
 
-Venujme minútku ticha naším drahým zosnulým gestám. Takto môžeme zhrnúť prácu s dvoma z našich údov- rukami. Dobre dobre. Je tam pokrok.
-Nevravím, že nie. Ale stále je to bieda. Nie sme v divadle, no nikto mi z hlavy nevytrieska, že istý "divadelný facelift" by nám predsa
-len bodol. Načo? Pre oživenie. Pre vtiahnutie, pre radosť, udržanie pozornosti a pre túúúúúú "prdel" ktorú to so sebou prináša. Naozaj.
-Občas pri tréningu popustím uzdu zmyslu pre drámu a rozšafnými gestami sa rozhadzujem po celej predsieni (inde veľké zrkadlo nemám). A
+Venujme minútku ticha, naším drahým, zosnulým gestám. Takto môžeme zhrnúť prácu s dvoma z našich údov- rukami. Dobre dobre. Je tam pokrok,
+nevravím, že nie. Ale stále je to bieda. Nie sme v divadle, no nikto mi z hlavy nevytrieska, že istý "divadelný facelift" by nám predsa
+len bodol. Načo? Pre oživenie. Pre vtiahnutie, pre radosť, udržanie pozornosti a pre túúúúúú "prdel" ktorú to so sebou nesie. Naozaj. Občas
+pri tréningu popustím uzdu zmyslu pre drámu a rozšafnými gestami sa rozhadzujem po celej predsieni (inde veľké zrkadlo nemáme). A
 má to zmysel.
 
 ![Interview](/)
 
 # ...po odmlke znova na doskách
 
-Mali sme dva prejavy. Juraj a ja. Obaja po dlhej pauze od posledného výstupu. U mňa cca dva mesiace. Juraj... takmer rok. Kto bol
-nervóznejší? Ale uhrali sme to. Je to ako v posilke. Vynecháš dva tri mesiace a prvé dva týždne končíš, kde si pred tým začínal.
-Juraj mal priblížiť svoje skúsenosti s mentoringom. Veľká pochvala. Rovnako ako rečníci na Speakathon-e pochopil dôležitosť príbehu. Na
-ňom vystaval prejav, ako bol prvým mentorom jeho otec a naučil ho chytať raky do pasce. Celé to zaobalil do vtipného nadhľadu prirovnaním
-kontextu k súčasnosti (dnes na to pozeráme optikou ničenia biotopu. Nesúďme tohto ekologického ignoranta veľmi prísne, naozaj to bola
-iná doba). Suma sumárum príjemné počúvanie, hodnotenie dostal, vie ako na tom je. Minúta ticha za Jurajove gestá bola, poďme ďalej. Pravdou
-je, že pri písani píspevku ubehli skoro dva týždne od jeho prejavu. Na raky si však stále spomínam. Tu vidíš, čo dokáže príbeh.
+Mali sme dva prejavy. Juraj a ja. Obaja po dlhej pauze od posledného výstupu. U mňa cca dva mesiace, Juraj... takmer rok. Kto bol nervóznejší?
+Ale uhrali sme to. Je to ako v posilke. Vynecháš dva tri mesiace a prvé dva týždne končíš, kde si pred tým začínal. Juraj mal priblížiť svoje
+skúsenosti s mentoringom. Veľká pochvala. Rovnako ako rečníci na Speakathon-e pochopil dôležitosť príbehu. Na ňom vystaval prejav. Ako bol
+prvým mentorom jeho otec a naučil ho chytať raky do pasce. Celé to podal vo vtipnom balení, prirovnaním kontextu k súčasnosti (dnes na to
+pozeráme optikou ničenia biotopu. Nesúďme tohto ekologického ignoranta veľmi prísne, naozaj to bola iná doba). Suma sumárum, príjemné počúvanie.
+Hodnotenie dostal, vie ako na tom je. Minúta ticha (aj) za Jurajove gestá bola, poďme ďalej. Pravdou je, že počas písania píspevku ubehli skoro
+dva týždne od jeho prejavu. Na raky si však stále spomínam. Tu vidíš, čo dokáže príbeh.
 
 ![Interview](/)
 
-Moja téma bola ladená mierne náučne. Priestor prejavov využívam na prezentovanie poznatkov, ako zlepšiť rozprávanie a vystupovanie na
-verejnosti. Každý prejav je venovaný inej časti (gestá, pauzy, príbeh, pohyby...). Toto spracovanie ide mimo projektov, ktoré musím splniť,
-tak uvidíme, ako dlho to vydrží. V prvom prejave série rozoberáme, ako z poslucháča urobiť pozorného poslucháča. Riešenie je jednoduché,
-potrebujeme ho emocionálne angažovať do problematiky prejavu. Čím to docieliť, si vieš pozrieť na videozázname (prístup má každý člen klubu).
-Ďalšie perličky poskytnem v nasledujúcich prejavoch. 7 minút, ktoré u nás máš na prejav, z ďaleka nestačí na vyčerpanie problematiky. Preto
-som zvolil formu mini modulov. Aby bol dodržaný ďalší zo základných pilierov dobrej prezentácie (informačná/zábavná rovina - aj tomu bude
+Moja téma bola ladená mierne edukačne. Priestor prejavov využívam na prezentovanie poznatkov, ako zlepšiť rozprávanie a vystupovanie (berte s
+rezervou. Nie som odborník, ale nadšenec :-)). Každý prejav je venovaný inej časti (gestá, pauzy, príbeh, pohyby...). Tieto témy idú mimo
+projektov, ktoré musím splniť. Uvidíme, ako dlho to vydrží. V prvom prejave série rozoberáme, ako z poslucháča urobiť pozorného poslucháča.
+Riešenie je jednoduché, potrebujeme ho emocionálne angažovať. Čím to docieliť, si vieš pozrieť na videozázname (prístup má každý člen klubu).
+Ďalšie perličky poskytnem v nasledujúcich prejavoch. 7 minút, ktoré na prejav máme, z ďaleka nestačí na vyčerpanie problematiky. Preto
+som zvolil formu "mini modulov". Aby bol dodržaný ďalší zo základných pilierov dobrej prezentácie (informačná verzus zábavná rovina - aj tomu bude
 minimálne jeden modul venovaný), niet inej cesty.
 
 ![Interview](/)
