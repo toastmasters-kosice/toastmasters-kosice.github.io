361b9353f3456a5a5e8dfe252e2541453c883b7a
Vladimír Záhradník <vladimir@zahradnik.io>
2024-02-12 22:20:08 +0100

    Update components

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,28 +1,28 @@
 import React from 'react'
 import { Link } from 'gatsby'
 
-import logo from '../img/logo-tmke.png'
-import email from '../img/social/email.svg'
-import facebook from '../img/social/facebook.svg'
-import linkedin from '../img/social/linkedin.svg'
-import discord from '../img/social/discord.svg'
-import youtube from '../img/social/youtube.svg'
-import github from '../img/github-icon.svg'
+import logo from "../img/logo-tmke.png";
+import email from "../img/social/email.svg";
+import facebook from "../img/social/facebook.svg";
+import linkedin from "../img/social/linkedin.svg";
+import discord from "../img/social/discord.svg";
+import youtube from "../img/social/youtube.svg";
+import github from "../img/github-icon.svg";
 
-const Footer = class extends React.Component {
-  render() {
-    return (
+const Footer = () => {
+  
+  return (
       <footer className="footer has-background-black has-text-white-ter">
         <div className="content has-text-centered">
           <img
             src={logo}
             alt="TM Košice"
-            style={{ width: '14em' }}
+            style={{ width: "14em", height: "10em" }}
           />
         </div>
         <div className="content has-text-centered has-background-black has-text-white-ter">
           <div className="container has-background-black has-text-white-ter">
-            <div style={{ maxWidth: '100vw' }} className="columns">
+            <div style={{ maxWidth: "100vw" }} className="columns">
               <div className="column is-4">
                 <section className="menu">
                   <ul className="menu-list">
@@ -123,8 +123,7 @@ const Footer = class extends React.Component {
           </div>
         </div>
       </footer>
-    )
-  }
-}
-
-export default Footer
+    );
+};
+  
+export default Footer;
